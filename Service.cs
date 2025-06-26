270412902181ab7864e38ade2d337ea16ff2a719
Rama Alkadour <ramaalkadour271@gmail.com>
2025-06-26 20:43:12 +0300

    jdfldasfd

--- Service.cs
+++ Service.cs
@@ -1,11 +1,8 @@
 public class Service
 {
-<<<<<<< HEAD
     public static Main()
     {
-        
+        Console.WriteLine("you dob");
     }
-=======
     Console.WriteLine("lio messi");
->>>>>>> 6a8335b0caf086a25400094c41952a74d2b1728c
 }
